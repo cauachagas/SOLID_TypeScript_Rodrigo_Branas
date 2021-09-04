8468a3ecedb9e77a8585fbd98499ca6b8ba51417
cauachagas <caua.geof@gmail.com>
2021-09-04 12:45:15 -0300

    Criando test para criar pedido

--- src/Order.test.ts
+++ src/Order.test.ts
@@ -1,3 +1,11 @@
-test("Deve criar um pedido", function(){
-    expect(1).toBe(1);
-})
+import Item from "./Item";
+import Order from "./Order";
+
+test("Deve criar um pedido", function () {
+  const order = new Order();
+  order.addItem(new Item("Beer", "Cacildes", 10));
+  order.addItem(new Item("Whisky", "Jack Daniels", 100));
+  order.addItem(new Item("Water", "Crystal", 1));
+  const total = order.getTotal();
+  expect(total).toBe(111);
+});
